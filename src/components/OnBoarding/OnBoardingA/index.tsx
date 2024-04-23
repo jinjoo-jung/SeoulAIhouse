aee9c81fc16d4fd5b422ccb35802ba647cd45fad
jinjoo-jung <wjdwlswn23@naver.com>
2024-04-23 16:25:59 +0900

    지도 api 셋팅

--- src/components/OnBoarding/OnBoardingA/index.tsx
+++ src/components/OnBoarding/OnBoardingA/index.tsx
@@ -1,22 +1,24 @@
 import React, { useEffect } from 'react';
 
-interface Window {
-  kakao: any; // `any` 대신 더 구체적인 타입을 사용할 수도 있습니다.
+declare global {
+  interface Window {
+    kakao: any;
+  }
 }
-const OnBoardingA = () => {
-  useEffect(() => {
-    const { kakao }: any = window; // 이제 TypeScript 오류가 발생하지 않습니다.
 
+const OnBoardingA = () => {
+  //처음 지도 그리기
+  useEffect(() => {
     const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
     const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
 
-    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
   }, []);
 
-  return <div id="map" style={{ width: '500px', height: '400px' }}></div>; // 높이 속성 오타 수정 ('400p' -> '400px')
+  return <div id="map" style={{ width: '500px', height: '400px' }}></div>;
 };
-
 export default OnBoardingA;
