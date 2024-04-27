f9e2ad0d1d21b2d9a23b9b15ed6e31cdc4859b80
jinjoo-jung <wjdwlswn23@naver.com>
2024-04-27 17:45:46 +0900

    recharts 설치

--- src/components/TownRecommend/RecommendItemList.tsx
+++ src/components/TownRecommend/RecommendItemList.tsx
@@ -1,22 +1,7 @@
 import React from 'react';
-import Clock from '../../assets/Clock.svg';
-import RecommendCommon from './RecommendCommon';
 
 const RecommendItemList = () => {
-  return (
-    <div>
-      <div>
-        <img src={Clock} alt="clock" />
-        <div>0분 ~ 60분</div>
-      </div>
-      <div>
-        <RecommendCommon />
-        <RecommendCommon />
-        <RecommendCommon />
-        <RecommendCommon />
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default RecommendItemList;
