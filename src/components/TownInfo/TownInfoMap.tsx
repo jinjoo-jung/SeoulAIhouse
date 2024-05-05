07e03569c6a4ab7558ac01a19b144f942f9b589e
jinjoo-jung <wjdwlswn23@naver.com>
2024-05-05 22:01:57 +0900

    지도 커스텀 마커 생성

--- src/components/TownInfo/TownInfoMap.tsx
+++ src/components/TownInfo/TownInfoMap.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import mapIcon from '../../assets/mapIcon.svg';
 import styled from '@emotion/styled';
 import mapRightIcon from '../../assets/mapRight.svg';
+import smileIcon from '../../assets/smileA.svg';
 
 declare global {
   interface Window {
@@ -10,16 +11,53 @@ declare global {
 }
 
 const TownInfoMap = () => {
-  const mapContainer = useRef(null);
   const { kakao } = window;
-  const position = new kakao.maps.LatLng(33.450701, 126.570667);
-  const mapOptions = {
-    center: position, // 지도의 중심좌표
-    level: 4, // 지도의 확대 레벨
-  };
+  const mapContainer = useRef(null);
 
   useEffect(() => {
+    const position = new kakao.maps.LatLng(37.54699, 127.09598);
+    const markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
+    const mapOptions = {
+      center: position, // 지도의 중심좌표
+      level: 4, // 지도의 확대 레벨
+    };
     const map = new kakao.maps.Map(mapContainer.current, mapOptions);
+    const marker = new kakao.maps.Marker({ position: markerPosition }); // 마커 생성
+
+    // const mapTypeControl = new kakao.maps.MapTypeControl();
+    // map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+    const zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+    //마커가 지도 위에 표시되는 설정
+    marker.setMap(map);
+
+    const content = `
+  <div class="customoverlay">
+  <div style="position:relative; display:flex; align-items:center;  " >
+  <img style ="position:absolute; margin-left: -8px;" src="${smileIcon}" />  
+  <div style =
+  "width:90px;
+  height:51px;
+  display:flex; 
+  align-items:center;
+  justify-content: center;
+  background-color: #F6F6F6;
+  font-size: 20px;
+  padding-left:48px;
+  border: 1px solid rgba(129, 129, 129, 0.4); /* 818181을 40% 투명도로 적용 */
+  border-radius: 100px;">80/500</div></div>
+</div>
+  `;
+
+    // 커스텀 오버레이 생성
+    new kakao.maps.CustomOverlay({
+      map,
+      position,
+      content,
+      yAnchor: 1,
+    });
   });
 
   return (
@@ -36,7 +74,7 @@ const TownInfoMap = () => {
           width: '1048px',
           height: '533px',
           display: 'block',
-          zIndex: '-1',
+          zIndex: '1',
         }}></div>
     </div>
   );
