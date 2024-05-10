5e3e55bc16420b35ab45cc7eac35e161c51a95c9
jinjoo-jung <wjdwlswn23@naver.com>
2024-05-10 09:59:35 +0900

    map content  개발

--- src/components/TownInfo/TownInfoMap.tsx
+++ src/components/TownInfo/TownInfoMap.tsx
@@ -5,6 +5,12 @@ import mapRightIcon from '../../assets/mapRight.svg';
 import normalIcon from '../../assets/noraml.svg';
 import smileIcon from '../../assets/smile.svg';
 import sadIcon from '../../assets/sad.svg';
+import miniNormal from '../../assets/miniNormal.svg';
+import miniSmile from '../../assets/miniSmile.svg';
+import miniSad from '../../assets/miniSad.svg';
+import questionIcon from '../../assets/question.svg';
+import arrow from '../../assets/mapArrow.svg';
+
 import { useNavigate } from 'react-router-dom';
 import getMapMarker from '../../api/getMapMaker';
 import { MapMarkerLabelResponse } from '../../types/mapMarker';
@@ -135,15 +141,29 @@ const TownInfoMap = () => {
           alt="mapRightIcon"
         />
       </HomeWrap>
-      <div
-        id="map"
-        ref={mapContainer}
-        style={{
-          width: '1048px',
-          height: '533px',
-          display: 'block',
-          zIndex: '1',
-        }}></div>
+      <MapContainer>
+        <MapStyle id="map" ref={mapContainer}></MapStyle>
+        <QuestionIcon src={questionIcon} alt="questionIcon" />
+        <ArrowIcon src={arrow} alt="arrow" />
+        <MapContentContainer>
+          <ContentText>예상 이동 시간에 따른 등급 분류</ContentText>
+          <MapIconContainer>
+            <MapIconWrap>
+              <img src={miniSmile} alt="miniSmile" />
+              <div> = 좋음 </div>
+            </MapIconWrap>
+            <MapIconWrap>
+              <img src={miniNormal} alt="miniNormal" />
+              <div> = 보통 </div>
+            </MapIconWrap>
+            <MapIconWrap>
+              <img src={miniSad} alt="miniSad" />
+              <div> = 나쁨 </div>
+            </MapIconWrap>
+          </MapIconContainer>
+          <SubText>*옆에 써 있는 숫자는 보증금 / 평균 월세입니다. </SubText>
+        </MapContentContainer>
+      </MapContainer>
     </div>
   );
 };
@@ -170,4 +190,75 @@ const HomeWrap = styled.div`
   img {
     cursor: pointer;
   }
+`;
+
+const MapContainer = styled.div`
+  position: relative;
+`;
+
+const MapStyle = styled.div`
+width: 1048px;
+height: 533px;
+display: block
+zIndex: 1;
+`;
+
+const QuestionIcon = styled.img`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  z-index: 1;
+  margin-bottom: 10px;
+  margin-right: 16px;
+`;
+
+const ArrowIcon = styled.img`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  z-index: 1;
+  margin-bottom: 80px;
+  margin-right: 80px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
+const MapContentContainer = styled.div`
+  width: 342px;
+  height: 141px;
+  border-radius: 20px;
+  background-color: #f6f6f6;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  position: absolute;
+  gap: 16px;
+  padding-left: 20px;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  margin-bottom: 60px;
+  margin-right: 100px;
+`;
+
+const ContentText = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+const SubText = styled.div`
+  font-size: 16px;
+  color: #8c8c8c;
+`;
+
+const MapIconContainer = styled.div`
+  display: flex;
+  gap: 28px;
+`;
+
+const MapIconWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
