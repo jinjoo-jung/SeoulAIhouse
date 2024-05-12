852ba04437c061de6a3b1678962463f05d09f632
jinjoo-jung <wjdwlswn23@naver.com>
2024-05-12 23:36:18 +0900

    로딩 컴포넌트 생성

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import styled from '@emotion/styled';
 import polygon from '../../assets/Polygon2.png';
 import miniLogo from '../../assets/seoul.svg';
+import { ClipLoader } from 'react-spinners';
+
+interface LoadingProps {
+  loading: boolean;
+}
 
 const Loading = () => {
   return (
     <OnBoardingAContainer>
       <LogoImg src={miniLogo} alt="miniLogo" />
+      <div>
+        <ClipLoader color="#005CAB" loading={true} size={130} />
+      </div>
       <MainText>
         우리의 AI가 당신에게 맞는 최적의 동네를 찾고 있습니다.
       </MainText>
-      <MainText>잠시만 기다려주세요.</MainText>
+      <SubText>잠시만 기다려주세요.</SubText>
     </OnBoardingAContainer>
   );
 };
@@ -36,6 +44,13 @@ const LogoImg = styled.img`
 `;
 
 const MainText = styled.div`
+  font-size: 20px;
+  font-weight: 500;
+  margin-top: 30px;
+  margin-bottom: 8px;
+`;
+
+const SubText = styled.div`
   font-size: 20px;
   font-weight: 500;
   margin: 8px;
